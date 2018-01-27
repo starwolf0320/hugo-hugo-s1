gohugoio/hugo

commit 6413559f7575e2653d76227a8037a7edbaae82aa

Add a way to disable one or more languages
This commit adds a new config setting:

```toml
disableLanguages = ["fr"]
```

If this is a multilingual site:

* No site for the French language will be created
* French content pages will be ignored/not read
* The French language configuration (menus etc.) will also be ignored

This makes it possible to start translating new languages and turn it on when you're happy etc.

Fixes #4297
Fixed #4329

commands/server.go
@@ -173,20 +173,23 @@ func server(cmd *cobra.Command, args []string) error {
 			c.Set("liveReloadPort", serverPorts[0])
 		}
 
-		if c.languages.IsMultihost() {
-			for i, language := range c.languages {
-				baseURL, err := fixURL(language, baseURL, serverPorts[i])
-				if err != nil {
-					return err
-				}
-				language.Set("baseURL", baseURL)
+		isMultiHost := c.languages.IsMultihost()
+		for i, language := range c.languages {
+			var serverPort int
+			if isMultiHost {
+				serverPort = serverPorts[i]
+			} else {
+				serverPort = serverPorts[0]
 			}
-		} else {
-			baseURL, err := fixURL(c.Cfg, baseURL, serverPorts[0])
+
+			baseURL, err := fixURL(language, baseURL, serverPort)
 			if err != nil {
 				return err
 			}
-			c.Set("baseURL", baseURL)
+			language.Set("baseURL", baseURL)
+			if i == 0 {
+				c.Set("baseURL", baseURL)
+			}
 		}
 
 		return nil

config/configProvider.go
@@ -20,6 +20,7 @@ type Provider interface {
 	GetBool(key string) bool
 	GetStringMap(key string) map[string]interface{}
 	GetStringMapString(key string) map[string]string
+	GetStringSlice(key string) []string
 	Get(key string) interface{}
 	Set(key string, value interface{})
 	IsSet(key string) bool

helpers/language.go
@@ -140,6 +140,11 @@ func (l *Language) GetStringMapString(key string) map[string]string {
 	return cast.ToStringMapString(l.Get(key))
 }
 
+//  returns the value associated with the key as a slice of strings.
+func (l *Language) GetStringSlice(key string) []string {
+	return cast.ToStringSlice(l.Get(key))
+}
+
 // Get returns a value associated with the key relying on specified language.
 // Get is case-insensitive for a key.
 //

hugolib/config.go
@@ -72,16 +72,46 @@ func LoadConfig(fs afero.Fs, relativeSourcePath, configFilename string) (*viper.
 }
 
 func loadLanguageSettings(cfg config.Provider, oldLangs helpers.Languages) error {
-	multilingual := cfg.GetStringMap("languages")
+
+	defaultLang := cfg.GetString("defaultContentLanguage")
+
+	var languages map[string]interface{}
+
+	languagesFromConfig := cfg.GetStringMap("languages")
+	disableLanguages := cfg.GetStringSlice("disableLanguages")
+
+	if len(disableLanguages) == 0 {
+		languages = languagesFromConfig
+	} else {
+		languages = make(map[string]interface{})
+		for k, v := range languagesFromConfig {
+			isDisabled := false
+			for _, disabled := range disableLanguages {
+				if disabled == defaultLang {
+					return fmt.Errorf("cannot disable default language %q", defaultLang)
+				}
+
+				if strings.EqualFold(k, disabled) {
+					isDisabled = true
+					break
+				}
+			}
+			if !isDisabled {
+				languages[k] = v
+			}
+
+		}
+	}
+
 	var (
 		langs helpers.Languages
 		err   error
 	)
 
-	if len(multilingual) == 0 {
+	if len(languages) == 0 {
 		langs = append(langs, helpers.NewDefaultLanguage(cfg))
 	} else {
-		langs, err = toSortedLanguages(cfg, multilingual)
+		langs, err = toSortedLanguages(cfg, languages)
 		if err != nil {
 			return fmt.Errorf("Failed to parse multilingual config: %s", err)
 		}
@@ -113,8 +143,6 @@ func loadLanguageSettings(cfg config.Provider, oldLangs helpers.Languages) error
 			}
 		}
 	}
-
-	defaultLang := cfg.GetString("defaultContentLanguage")
 
 	// The defaultContentLanguage is something the user has to decide, but it needs
 	// to match a language in the language definition list.

hugolib/fileInfo.go
@@ -31,6 +31,9 @@ type fileInfo struct {
 	bundleTp bundleDirType
 	source.ReadableFile
 	overriddenLang string
+
+	// Set if the content language for this file is disabled.
+	disabled bool
 }
 
 func (fi *fileInfo) Lang() string {
@@ -59,6 +62,9 @@ func newFileInfo(sp *source.SourceSpec, baseDir, filename string, fi os.FileInfo
 		bundleTp:     tp,
 		ReadableFile: baseFi,
 	}
+
+	lang := f.Lang()
+	f.disabled = lang != "" && sp.DisabledLanguages[lang]
 
 	return f
 

hugolib/page_bundler_capture.go
@@ -149,8 +149,10 @@ func (c *capturer) capturePartial(filenames ...string) error {
 			// create the proper mapping for it.
 			c.getRealFileInfo(dir)
 
-			f := c.newFileInfo(resolvedFilename, fi, tp)
-			c.copyOrHandleSingle(f)
+			f, active := c.newFileInfo(resolvedFilename, fi, tp)
+			if active {
+				c.copyOrHandleSingle(f)
+			}
 		}
 	}
 
@@ -228,7 +230,10 @@ func (c *capturer) handleBranchDir(dirname string) error {
 
 		tp, isContent := classifyBundledFile(fi.Name())
 
-		f := c.newFileInfo(fi.filename, fi.FileInfo, tp)
+		f, active := c.newFileInfo(fi.filename, fi.FileInfo, tp)
+		if !active {
+			continue
+		}
 		if f.isOwner() {
 			dirs.addBundleHeader(f)
 		} else if !isContent {
@@ -309,7 +314,7 @@ func (c *capturer) handleDir(dirname string) error {
 		return c.handleNonBundle(dirname, files, state == dirStateSinglesOnly)
 	}
 
-	var fileInfos = make([]*fileInfo, len(files))
+	var fileInfos = make([]*fileInfo, 0, len(files))
 
 	for i, fi := range files {
 		currentType := bundleNot
@@ -324,8 +329,12 @@ func (c *capturer) handleDir(dirname string) error {
 		if bundleType == bundleNot && currentType != bundleNot {
 			bundleType = currentType
 		}
+		f, active := c.newFileInfo(fi.filename, fi.FileInfo, currentType)
+		if !active {
+			continue
+		}
 
-		fileInfos[i] = c.newFileInfo(fi.filename, fi.FileInfo, currentType)
+		fileInfos = append(fileInfos, f)
 	}
 
 	var todo []*fileInfo
@@ -377,8 +386,11 @@ func (c *capturer) handleNonBundle(
 			}
 		} else {
 			if singlesOnly {
-				file := c.newFileInfo(fi.filename, fi, bundleNot)
-				c.handler.handleSingles(file)
+				f, active := c.newFileInfo(fi.filename, fi, bundleNot)
+				if !active {
+					continue
+				}
+				c.handler.handleSingles(f)
 			} else {
 				c.handler.handleCopyFiles(fi.filename)
 			}
@@ -462,7 +474,10 @@ func (c *capturer) collectFiles(dirname string, handleFiles func(fis ...*fileInf
 				return err
 			}
 		} else {
-			handleFiles(c.newFileInfo(fi.filename, fi.FileInfo, bundleNot))
+			f, active := c.newFileInfo(fi.filename, fi.FileInfo, bundleNot)
+			if active {
+				handleFiles(f)
+			}
 		}
 	}
 
@@ -506,8 +521,9 @@ func (c *capturer) readDir(dirname string) ([]fileInfoName, error) {
 	return fis, nil
 }
 
-func (c *capturer) newFileInfo(filename string, fi os.FileInfo, tp bundleDirType) *fileInfo {
-	return newFileInfo(c.sourceSpec, c.baseDir, filename, fi, tp)
+func (c *capturer) newFileInfo(filename string, fi os.FileInfo, tp bundleDirType) (*fileInfo, bool) {
+	f := newFileInfo(c.sourceSpec, c.baseDir, filename, fi, tp)
+	return f, !f.disabled
 }
 
 type fileInfoName struct {

hugolib/page_bundler_capture_test.go
@@ -174,6 +174,7 @@ func TestPageBundlerCaptureMultilingual(t *testing.T) {
 	expected := `
 F:
 /work/base/1s/mypage.md
+/work/base/1s/mypage.nn.md
 /work/base/bb/_1.md
 /work/base/bb/_1.nn.md
 /work/base/bb/en.md

hugolib/page_bundler_test.go
@@ -192,6 +192,10 @@ func TestPageBundlerSiteMultilingual(t *testing.T) {
 
 				s := sites.Sites[0]
 
+				assert.Equal(8, len(s.RegularPages))
+				assert.Equal(18, len(s.Pages))
+				assert.Equal(35, len(s.AllPages))
+
 				bundleWithSubPath := s.getPage(KindPage, "lb/index")
 				assert.NotNil(bundleWithSubPath)
 
@@ -214,6 +218,8 @@ func TestPageBundlerSiteMultilingual(t *testing.T) {
 				assert.Equal(bfBundle, s.getPage(KindPage, "my-bf-bundle"))
 
 				nnSite := sites.Sites[1]
+				assert.Equal(7, len(nnSite.RegularPages))
+
 				bfBundleNN := nnSite.getPage(KindPage, "bf/my-bf-bundle/index")
 				assert.NotNil(bfBundleNN)
 				assert.Equal("nn", bfBundleNN.Lang())
@@ -231,6 +237,48 @@ func TestPageBundlerSiteMultilingual(t *testing.T) {
 
 			})
 	}
+}
+
+func TestMultilingualDisableDefaultLanguage(t *testing.T) {
+	t.Parallel()
+
+	assert := require.New(t)
+	cfg, _ := newTestBundleSourcesMultilingual(t)
+
+	cfg.Set("disableLanguages", []string{"en"})
+
+	err := loadDefaultSettingsFor(cfg)
+	assert.Error(err)
+	assert.Contains(err.Error(), "cannot disable default language")
+}
+
+func TestMultilingualDisableLanguage(t *testing.T) {
+	t.Parallel()
+
+	assert := require.New(t)
+	cfg, fs := newTestBundleSourcesMultilingual(t)
+	cfg.Set("disableLanguages", []string{"nn"})
+
+	assert.NoError(loadDefaultSettingsFor(cfg))
+	sites, err := NewHugoSites(deps.DepsCfg{Fs: fs, Cfg: cfg})
+	assert.NoError(err)
+	assert.Equal(1, len(sites.Sites))
+
+	assert.NoError(sites.Build(BuildCfg{}))
+
+	s := sites.Sites[0]
+
+	assert.Equal(8, len(s.RegularPages))
+	assert.Equal(18, len(s.Pages))
+	// No nn pages
+	assert.Equal(18, len(s.AllPages))
+	for _, p := range s.rawAllPages {
+		assert.True(p.Lang() != "nn")
+	}
+	for _, p := range s.AllPages {
+		assert.True(p.Lang() != "nn")
+	}
+
 }
 
 func TestPageBundlerSiteWitSymbolicLinksInContent(t *testing.T) {
@@ -509,6 +557,7 @@ TheContent.
 	writeSource(t, fs, filepath.Join(workDir, "layouts", "_default", "list.html"), layout)
 
 	writeSource(t, fs, filepath.Join(workDir, "base", "1s", "mypage.md"), pageContent)
+	writeSource(t, fs, filepath.Join(workDir, "base", "1s", "mypage.nn.md"), pageContent)
 	writeSource(t, fs, filepath.Join(workDir, "base", "1s", "mylogo.png"), "content")
 
 	writeSource(t, fs, filepath.Join(workDir, "base", "bb", "_index.md"), pageContent)

starwolf0320/hugo-hugo-s1

commit dcd10a62b3a511a61984f7c2626bd608c1b27613

commands: Fix baseURL server regression for multilingual sites
This was introduced in 6413559f7575e2653d76227a8037a7edbaae82aa a couple of days ago, and demonstrates that we really need better tests for the server/commands package.

Fixes #4333

commands/server.go
@@ -186,7 +186,9 @@ func server(cmd *cobra.Command, args []string) error {
 			if err != nil {
 				return err
 			}
-			language.Set("baseURL", baseURL)
+			if isMultiHost {
+				language.Set("baseURL", baseURL)
+			}
 			if i == 0 {
 				c.Set("baseURL", baseURL)
 			}